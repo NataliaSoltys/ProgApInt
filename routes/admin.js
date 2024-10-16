97d52d6c9b655616d801661cc7cf2ae09e3fb27c
NataliaSoltys <nataliasoltys00@gmail.com>
2024-10-16 15:51:32 +0200

    com

--- routes/admin.js
+++ routes/admin.js
@@ -5,8 +5,20 @@ const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(path.dirname(require.main.filename), 
-  'views', 'add-product.html'));
+    res.render('add-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        formsCSS: true,
+        productCSS: true,
+        activeAddProduct: true
+    });
+});
+
+router.post('/add-product', (req, res, next) => {
+    const price = Math.floor(Math.random() * 90) + 10;
+    const desc = 'A very nice ' + req.body.title;
+    products.push({ title: req.body.title, price: price, desc: desc });
+    res.redirect('/');
 });
 
 // /admin/add-product => POST
